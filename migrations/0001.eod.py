ad42103232ddb3b1776757cc0844fcfd9045a995
Jeremy Fergason <jeremy.fergason@gmail.com>
2016-04-24 16:01:15 -0700

    Add volume column

--- migrations/0001.eod.py
+++ migrations/0001.eod.py
@@ -6,12 +6,13 @@ step(
     """
     CREATE TABLE eod (
         date DATE NOT NULL,
-        security INTEGER REFERENCES security(id) ON DELETE CASCADE,
-        open DECIMAL(12, 4) NOT NULL,
-        close DECIMAL(12, 4) NOT NULL,
-        adj_close DECIMAL(12, 4) NOT NULL,
-        high DECIMAL(12, 4) NOT NULL,
-        low DECIMAL(12, 4) NOT NULL,
+        security INTEGER NOT NULL REFERENCES security(id) ON DELETE CASCADE,
+        open DECIMAL(12, 4),
+        close DECIMAL(12, 4),
+        adj_close DECIMAL(12, 4),
+        high DECIMAL(12, 4),
+        low DECIMAL(12, 4),
+        volume bigint,
         source INTEGER REFERENCES source(id) ON DELETE CASCADE
     )
     """,
