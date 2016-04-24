ef4e9b568cf6261f08a7d0c2737505e6671025dd
Jeremy Fergason <jeremy.fergason@gmail.com>
2016-04-24 14:48:21 -0700

    initial import

---
+++ migrations/0001.eod.py
@@ -0,0 +1,19 @@
+from yoyo import step
+
+__depends__ = ['0001.security',]
+
+step(
+    """
+    CREATE TABLE eod (
+        date DATE NOT NULL,
+        symbol INTEGER REFERENCES security(id) ON DELETE CASCADE,
+        open DECIMAL(12, 4) NOT NULL,
+        close DECIMAL(12, 4) NOT NULL,
+        adj_close DECIMAL(12, 4) NOT NULL,
+        high DECIMAL(12, 4) NOT NULL,
+        low DECIMAL(12, 4) NOT NULL,
+        source INTEGER REFERENCES source(id) ON DELETE CASCADE
+    )
+    """,
+    "DROP TABLE eod"
+)
