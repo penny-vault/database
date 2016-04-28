64c9481387c219f4db26fa6d0b935ea04a8eb203
Fedora Cloud User <fedora@ip-10-0-0-151.us-west-2.compute.internal>
2016-04-28 07:29:56 +0000

    Fix typos in migration.

--- migrations/0004.security.py
+++ migrations/0004.security.py
@@ -1,13 +1,7 @@
 from yoyo import step
 
-__depends__ = ['0003.security.py']
+__depends__ = ['0003.security']
 
-step(
-    """
-    ALTER TABLE security ADD corporation REFERENCES corporation(id) ON DELETE CASCADE
-    """,
-    "ALTER TABLE security DROP corporation"
-)
 step(
     """
     ALTER TABLE security ADD name VARCHAR(128)
