092d1d5bd2d19d8bde6deba83438da1fa3f30ad8
Fedora Cloud User <fedora@ip-10-0-0-151.us-west-2.compute.internal>
2016-04-30 18:09:29 +0000

    Fix SQL errors

--- migrations/0006.security.py
+++ migrations/0006.security.py
@@ -4,13 +4,7 @@ __depends__ = ['0005.security']
 
 step(
     """
-    ALTER TABLE security ADD corporation REFERENCES corporation(id) ON DELETE CASCADE
+    ALTER TABLE security ADD corporation INTEGER REFERENCES corporation(id) ON DELETE CASCADE
     """,
     "ALTER TABLE security DROP corporation"
-)
-step(
-    """
-    ALTER TABLE security ADD name VARCHAR(128)
-    """,
-    "ALTER TABLE security DROP name"
 )
