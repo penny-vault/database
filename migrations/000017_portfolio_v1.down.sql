9c1cd90b332e604d8b42ebf2dd07fd2354bfe165
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-09-07 19:42:03 -0700

    Add temporary column to portfolio_v1

--- migrations/000017_portfolio_v1.down.sql
+++ migrations/000017_portfolio_v1.down.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 ALTER TABLE portfolio_v1 ALTER COLUMN user_id DROP DEFAULT;
-ALTER TABLE portfolio_v1 DROP COLUMN temporary BOOLEAN;
+ALTER TABLE portfolio_v1 DROP COLUMN temporary;
+ALTER TABLE portfolio_v1 DROP COLUMN benchmark;
 
 COMMIT;
