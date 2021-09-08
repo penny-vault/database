74217d8139929eeed1cb686e709576c119a5ec47
Jeremy Fergason <jeremy.fergason@corwynn.com>
2021-09-07 19:42:34 -0700

    add temporary column

--- migrations/000017_portfolio_v1.up.sql
+++ migrations/000017_portfolio_v1.up.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 ALTER TABLE portfolio_v1 ALTER COLUMN user_id SET DEFAULT current_user;
-ALTER TABLE portfolio_v1 ADD COLUMN temporary BOOLEAN DEFAULT false;
+ALTER TABLE portfolio_v1 ADD COLUMN temporary BOOLEAN DEFAULT true;
+ALTER TABLE portfolio_v1 ADD COLUMN benchmark TEXT DEFAULT 'VFINX';
 
 COMMIT;
