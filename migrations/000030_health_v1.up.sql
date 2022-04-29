0a7b48545118269d510fcf2ffe46301c5e4e6f75
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-04-29 00:10:53 -0700

    Remove risk_indicators from health view

---
+++ migrations/000030_health_v1.up.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+DROP VIEW health_v1;
+
+CREATE VIEW health_v1 AS
+    SELECT
+        (SELECT (now() - INTERVAL '32 hours')::date) AS date,
+        to_char((now() - INTERVAL '30 hours')::date, 'Day') as day_of_week,
+        CASE WHEN (EXTRACT(isodow FROM (now() - INTERVAL '32 hours')::date) IN (6, 7)) THEN 2
+             WHEN ((SELECT count(*) FROM eod_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 35000 AND
+                   (SELECT count(*) FROM seeking_alpha_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 3500 AND
+                   (SELECT count(*) FROM zacks_financials_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 3500) THEN 1
+             ELSE 0
+        END AS status,
+        (SELECT count(*) FROM eod_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS eod_v1_cnt,
+        (SELECT count(*) FROM seeking_alpha_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS seeking_alpha_v1_cnt,
+        (SELECT count(*) FROM zacks_financials_v1 WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS zacks_financials_v1_cnt;
+
+CREATE ROLE pvhealth WITH nologin;
+GRANT pvhealth TO pvapi;
+GRANT SELECT ON health_v1 TO pvhealth;
+
+COMMIT;
