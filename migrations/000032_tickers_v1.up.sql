e61369e3b9eeab7b876fa8e7ddefb6973372f3d2
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-05-18 07:36:00 -0700

    Fix references in health table in migration 32

--- migrations/000032_tickers_v1.up.sql
+++ migrations/000032_tickers_v1.up.sql
@@ -93,13 +93,11 @@ CREATE VIEW health AS
         to_char((now() - INTERVAL '30 hours')::date, 'Day') as day_of_week,
         CASE WHEN (EXTRACT(isodow FROM (now() - INTERVAL '32 hours')::date) IN (6, 7)) THEN 2
              WHEN ((SELECT count(*) FROM eod WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 35000 AND
-                   (SELECT count(*) FROM zacks_financials WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 3500 AND
-                   (SELECT count(*) FROM risk_indicators WHERE event_date>(select (now() - INTERVAL '60 hours')::date)) >= 1) THEN 1
+                   (SELECT count(*) FROM zacks_financials WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) > 3500) THEN 1
              ELSE 0
         END AS status,
-        (SELECT count(*) FROM eod WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS eod_v1_cnt,
-        (SELECT count(*) FROM zacks_financials WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS zacks_financials_v1_cnt,
-        (SELECT count(*) FROM risk_indicators WHERE event_date>(select (now() - INTERVAL '60 hours')::date)) AS risk_indicators_v1_cnt;
+        (SELECT count(*) FROM eod WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS eod_cnt,
+        (SELECT count(*) FROM zacks_financials WHERE event_date=(select (now() - INTERVAL '32 hours')::date)) AS zacks_financials_cnt;
 
 GRANT SELECT ON health TO pvhealth;
 
