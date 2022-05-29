75cf3f3324c7c61c9e34a466af96f7966a87dfb8
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-05-28 22:11:47 -0700

    add asset type for fred

--- migrations/000034_pvsource.up.sql
+++ migrations/000034_pvsource.up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
 ALTER TYPE datasource ADD VALUE IF NOT EXISTS 'api.pennyvault.com' AFTER 'api.tiingo.com';
+ALTER TYPE assettype ADD VALUE IF NOT EXISTS 'FRED' AFTER 'American Depository Receipt Common';
 
 COMMIT;
